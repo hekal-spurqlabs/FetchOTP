a1cf0d93d885bdc9be8192bedae69247117e06f0
hekal-spurqlabs <harish.ekal@spurqlabs.com>
2024-05-30 17:31:17 +0530

    requirement.txt file added

--- FetchContentFromGmail.py
+++ FetchContentFromGmail.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from imap_tools import MailBox, AND
 import os
+import re
 
 # Load .env file
 load_dotenv()
@@ -20,49 +21,47 @@ def check_latest_email():
             return None, None, None  # No Emails Found
         return emails[0]
 
-def extract_link(email_text): 
 
+def extract_link(email_text):
     # Regex pattern to match URLs 
 
-    url_pattern = re.compile(r'https?://[^\s]+') 
+    url_pattern = re.compile(r'https?://[^\s]+')
 
-    match = url_pattern.search(email_text) 
+    match = url_pattern.search(email_text)
 
-    if match: 
+    if match:
+        return match.group()
 
-        return match.group() 
+    return None
 
-    return None 
 
-def extract_otp(email_text): 
-
+def extract_otp(email_text):
     # Regex pattern to match a 6-digit number 
 
-    otp_pattern = re.compile(r'\b\d{6}\b') 
+    otp_pattern = re.compile(r'\b\d{6}\b')
 
-    match = otp_pattern.search(email_text) 
+    match = otp_pattern.search(email_text)
 
-    if match: 
+    if match:
+        return match.group()
 
-        return match.group() 
-
-    return None 
-
+    return None
 
 
 if __name__ == "__main__":
     email = check_latest_email()
-    print("email subject: ",email.subject)
+    print("email subject: ", email.subject)
     print(email.text)
     print(email.from_)
     # To Fetch Email Link
     link = extract_link(email.text)
-    if link: 
-      print("Extracted Link: ", link) 
-    else: 
-      print("No link found in the email content.")
+    if link:
+        print("Extracted Link: ", link)
+    else:
+        print("No link found in the email content.")
     # To extract otp from email
-    if otp: 
-      print("Extracted OTP: ", otp) 
-    else: 
-      print("No OTP found in the email content.")
+    otp = extract_otp(email.text)
+    if otp:
+        print("Extracted OTP: ", otp)
+    else:
+        print("No OTP found in the email content.")
